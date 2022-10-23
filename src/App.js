0a98079ee464ea938ebcf521fd7dfc4edf6b0130
Reigneir <reneir.panares@usjr.edu.ph>
2022-10-23 22:42:23 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+const Dashboard = (props) => { // props encapsulate the token and logout method
+
+	const [, setToken] = useState(props ? props.token : ''); // check if props exists. if ys, store the token 
+	
+	const onLogout = (evt) => {
+		const { logout } = props; // deconstruct the object props and extract only the logout method
+		setToken('');
+		if(logout) {  // check if prop logout exist
+			logout(); // if yes, trigger handleLogout method in Login component
+		}
+	};
+
+	return(
+		<>
+			<h2>Users Table</h2>
+			<table>
+        <tr>
+          <th>Id </th>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Action</th>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Leanne Graham</td>
+          <td>Bret</td>
+          <td>Sincere@april.biz</td>
+          <td>1-770-736-8031 x56442</td>
+          <td><a href='#\' onclick={onLogout}>View Details</a></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td></td>
+          <td>Bret</td>
+          <td>Sincere@april.biz</td>
+          <td>1-770-736-8031 x56442</td>
+          <td><a href='#\' onclick={onLogout}>View Details</a></td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Leanne Graham</td>
+          <td>Bret</td>
+          <td>Sincere@april.biz</td>
+          <td>1-770-736-8031 x56442</td>
+          <td><a href='#\' onclick={onLogout}>View Details</a></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Leanne Graham</td>
+          <td>Bret</td>
+          <td>Sincere@april.biz</td>
+          <td>1-770-736-8031 x56442</td>
+          <td><a href='#\' onclick={onLogout}>View Details</a></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Leanne Graham</td>
+          <td>Bret</td>
+          <td>Sincere@april.biz</td>
+          <td>1-770-736-8031 x56442</td>
+          <td><a href='#\' onclick={onLogout}>View Details</a></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Leanne Graham</td>
+          <td>Bret</td>
+          <td>Sincere@april.biz</td>
+          <td>1-770-736-8031 x56442</td>
+          <td><a href='#\' onclick={onLogout}>View Details</a></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Leanne Graham</td>
+          <td>Bret</td>
+          <td>Sincere@april.biz</td>
+          <td>1-770-736-8031 x56442</td>
+          <td><a href='#\' onclick={onLogout}>View Details</a></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Leanne Graham</td>
+          <td>Bret</td>
+          <td>Sincere@april.biz</td>
+          <td>1-770-736-8031 x56442</td>
+          <td><a href='#\' onclick={onLogout}>View Details</a></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Leanne Graham</td>
+          <td>Bret</td>
+          <td>Sincere@april.biz</td>
+          <td>1-770-736-8031 x56442</td>
+          <td><a href='#\' onclick={onLogout}>View Details</a></td>
+        </tr>
+        <tr>
+          <td>1</td>
+          <td>Leanne Graham</td>
+          <td>Bret</td>
+          <td>Sincere@april.biz</td>
+          <td>1-770-736-8031 x56442</td>
+          <td><a href='#\' onclick={onLogout}>View Details</a></td>
+        </tr>
+				{/* <li >user {token}</li>
+				<li><a href='#\' onClick={onLogout}>Logout</a></li> */}
+        </table>
+			{/* <h2>Users</h2>
+			<h4>Table here...</h4> */}
+		</>
+	)
+};
+
+export default Dashboard;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { useState } from "react";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const Dashboard = (props) => { // props encapsulate the token and logout method
+
+	const [token, setToken] = useState(props ? props.token : ''); // check if props exists. if ys, store the token 
+	
+	const onLogout = (evt) => {
+		const { logout } = props; // deconstruct the object props and extract only the logout method
+		setToken('');
+		if(logout) {  // check if prop logout exist
+			logout(); // if yes, trigger handleLogout method in Login component
+		}
+	};
+
+	return(
+		<>
+			<h1>Menu</h1>
+			<ul>
+				<li>user {token}</li>
+				<li><a href='#\' onClick={onLogout}>Logout</a></li>
+			</ul>
+			<h2>Users</h2>
+			<h4>Table here...</h4>
+		</>
+	)
+};
+
+export default Dashboard;
