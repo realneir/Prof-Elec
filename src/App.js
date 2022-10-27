95cb6c39bb95b7f814a27bc2d99abd04baddd627
Reigneir <reneir.panares@usjr.edu.ph>
2022-10-27 11:10:03 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,117 +1,240 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import './App.css';
+import './Login.css';
+import './UserTable.css';
+import './UserCard.css';
 
+const Project = () => {
 
-const Dashboard = (props) => { // props encapsulate the token and logout method
+	const [result, setResult] = useState('');
+	const [detail, setDetail] = useState([]);
+	const [screen, setScreen] = useState(!true);
+	const [accDet, setaccDet] = useState(0);
+	const [viewDetail, setViewDetail] = useState(!true);
+	const [totalUsers, setTotalUsers] = useState(0);
 
-	const [, setToken] = useState(props ? props.token : ''); // check if props exists. if ys, store the token 
-	
-	const onLogout = (evt) => {
-		const { logout } = props; // deconstruct the object props and extract only the logout method
-		setToken('');
-		if(logout) {  // check if prop logout exist
-			logout(); // if yes, trigger handleLogout method in Login component
-		}
+	const [user, setUser] = useState({
+		email: '',
+		password: ''
+	});
+
+	const onChange = (evt) => {
+		const value = evt.target.value;
+		const name = evt.target.name;
+		setUser({
+			...user,
+			[name]: value
+		});
 	};
 
-	return(
-		<>
-			<h2>Users Table</h2>
-			<table>
-        <tr>
-          <th>Id </th>
-          <th>Name</th>
-          <th>Username</th>
-          <th>Email</th>
-          <th>Phone</th>
-          <th>Action</th>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Leanne Graham</td>
-          <td>Bret</td>
-          <td>Sincere@april.biz</td>
-          <td>1-770-736-8031 x56442</td>
-          <td><a href='#\' onclick={onLogout}>View Details</a></td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td></td>
-          <td>Bret</td>
-          <td>Sincere@april.biz</td>
-          <td>1-770-736-8031 x56442</td>
-          <td><a href='#\' onclick={onLogout}>View Details</a></td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Leanne Graham</td>
-          <td>Bret</td>
-          <td>Sincere@april.biz</td>
-          <td>1-770-736-8031 x56442</td>
-          <td><a href='#\' onclick={onLogout}>View Details</a></td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Leanne Graham</td>
-          <td>Bret</td>
-          <td>Sincere@april.biz</td>
-          <td>1-770-736-8031 x56442</td>
-          <td><a href='#\' onclick={onLogout}>View Details</a></td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Leanne Graham</td>
-          <td>Bret</td>
-          <td>Sincere@april.biz</td>
-          <td>1-770-736-8031 x56442</td>
-          <td><a href='#\' onclick={onLogout}>View Details</a></td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Leanne Graham</td>
-          <td>Bret</td>
-          <td>Sincere@april.biz</td>
-          <td>1-770-736-8031 x56442</td>
-          <td><a href='#\' onclick={onLogout}>View Details</a></td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Leanne Graham</td>
-          <td>Bret</td>
-          <td>Sincere@april.biz</td>
-          <td>1-770-736-8031 x56442</td>
-          <td><a href='#\' onclick={onLogout}>View Details</a></td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Leanne Graham</td>
-          <td>Bret</td>
-          <td>Sincere@april.biz</td>
-          <td>1-770-736-8031 x56442</td>
-          <td><a href='#\' onclick={onLogout}>View Details</a></td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Leanne Graham</td>
-          <td>Bret</td>
-          <td>Sincere@april.biz</td>
-          <td>1-770-736-8031 x56442</td>
-          <td><a href='#\' onclick={onLogout}>View Details</a></td>
-        </tr>
-        <tr>
-          <td>1</td>
-          <td>Leanne Graham</td>
-          <td>Bret</td>
-          <td>Sincere@april.biz</td>
-          <td>1-770-736-8031 x56442</td>
-          <td><a href='#\' onclick={onLogout}>View Details</a></td>
-        </tr>
-				{/* <li >user {token}</li>
-				<li><a href='#\' onClick={onLogout}>Logout</a></li> */}
-        </table>
-			{/* <h2>Users</h2>
-			<h4>Table here...</h4> */}
-		</>
-	)
+	const fetchData = async () => {
+		const response = await fetch(
+			"https://jsonplaceholder.typicode.com/users"
+		).then((response) => response.json());
+		setDetail(response);
+		setTotalUsers(response.length);
+	};
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const sendLogin = async (user) => {
+		console.log(user);
+		//"email": "eve.holt@reqres.in",
+		//"password": "cityslicka"
+		const url = 'https://reqres.in/api/login';
+		const params = {
+			"email": user.email,
+			"password": user.password
+		}
+
+		fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(params),
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				if (data && data.token) {
+					setResult(data.token);
+					setScreen(true);
+				} else {
+					setResult('Error login. Please verify.')
+				}
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
+	};
+
+	const handleUserLogin = () => {
+		sendLogin(user);
+	};
+
+	const handleLogOut = () => {
+		setScreen(false);
+		setResult('');
+	}
+
+	const menu =
+		['Id', 'Name', 'Username', 'Email', 'Phone', 'Action'];
+
+	const Dashboard = menu.map
+		(
+			menu =>
+				<th> {menu} </th>
+		);
+
+	const userInfo = detail.map
+		(
+			det =>
+				<tr>
+					<td>{det.id}</td>
+					<td>{det.name}</td>
+					<td>{det.username}</td>
+					<td>{det.email}</td>
+					<td>{det.phone}</td>
+					<td>
+						<button onClick={() => {
+							setaccDet(det.id - 1);
+							setViewDetail(true);
+						}
+						}> View Details </button>
+					</td>
+				</tr>
+		);
+
+	function ViewDetail() {
+		if (!viewDetail) {
+			return (
+				<>
+					<h1 id="user-title">Users Table</h1>
+					<div className="table-container">
+						<table>
+							<tr>
+								{Dashboard}
+							</tr>
+							{userInfo}
+						</table>
+					</div>
+					<h2 data-testid="total-users">Total Users: {totalUsers}</h2>
+				</>
+			);
+		}
+		else {
+			return (
+				<>
+					<br />
+
+
+					<table>
+						<tr>
+							<td>Id:</td>
+							<td><b>{detail[accDet].id}</b></td>
+						</tr>
+						<tr>
+							<td>Name:</td>
+							<td><b>{detail[accDet].name}</b></td>
+						</tr>
+						<tr>
+							<td>Address:</td>
+							<td><b>
+								{`${detail[accDet].address.street} 
+               	 				${detail[accDet].address.suite}
+								  ${detail[accDet].address.city} 
+                				${detail[accDet].address.zipcode}`}</b></td>
+						</tr>
+						<tr>
+							<td>Company:</td>
+							<td><b>{detail[accDet].company.name}</b></td>
+						</tr>
+					</table>
+					
+
+
+					<br />
+					<button onClick={() =>
+						setViewDetail(!true)}>
+						Back to table</button>
+				</>
+			);
+		}
+	}
+
+	if (!screen) {
+		return (
+			<>
+				<div className="container">
+					<div className="login-form">
+						<div className="login">
+							<div className="login-label">
+								<label htmlFor="income">Enter Email<label className="asterisk"> *</label></label>
+							</div>
+							<input
+								placeholder="eve.holt@regres.in"
+								type="text"
+								onChange={onChange}
+								data-testid="email"
+								name="email"
+								className="login-input"
+							/>
+						</div>
+						<div className="login">
+							<div className="login-label">
+								<label htmlFor="income">Enter Password (cityslicka)<label className="asterisk"> *</label></label>
+							</div>
+							<input
+								placeholder="*********"
+								type="password"
+								onChange={onChange}
+								data-testid="password"
+								name="password"
+								className="login-input"
+							/>
+						</div>
+
+						<div className="container-login-btn">
+							<button
+								onClick={handleUserLogin}
+								data-testid="user-login"
+							>
+								Login
+							</button>
+						</div>
+					</div>
+				</div>
+
+				<div className="container-server">
+					<h3 data-testid="login-verify"><b>Server Reply:</b> <i>{result}</i></h3>
+				</div>
+
+			</>
+		)
+	} else {
+		return (
+			detail ? (
+				<><br />
+
+					<div className="header-menu">
+						Token: {result}
+						<button id="logout" onClick={handleLogOut}>
+							Logout
+						</button>
+
+					</div>
+
+					<div className="user-detail">
+						<ViewDetail />
+					</div>
+				</>
+			) :
+				(
+					<p>Fetching...</p>
+				)
+		);
+	}
 };
 
-export default Dashboard;
+export default Project;
--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,75 @@
-import { useState } from "react";
+import { render, screen, cleanup, waitFor, fireEvent } from "@testing-library/react";
+import Project from "./App";
 
-const Dashboard = (props) => { // props encapsulate the token and logout method
+describe("Check the UI.",()=>{
+	it("has an input for email",()=>{
+		render(<Project />);
+		const email = screen.getByTestId("email");
+		expect(email).toBeInTheDocument();
+	});
 
-	const [token, setToken] = useState(props ? props.token : ''); // check if props exists. if ys, store the token 
-	
-	const onLogout = (evt) => {
-		const { logout } = props; // deconstruct the object props and extract only the logout method
-		setToken('');
-		if(logout) {  // check if prop logout exist
-			logout(); // if yes, trigger handleLogout method in Login component
-		}
-	};
+	it("has an input for email type text",()=>{
+		render(<Project />);
+		const input = screen.getByTestId("email");
+		expect(input).toBeInTheDocument();
+		expect(input).toHaveAttribute("type", "text");
+	});
 
-	return(
-		<>
-			<h1>Menu</h1>
-			<ul>
-				<li>user {token}</li>
-				<li><a href='#\' onClick={onLogout}>Logout</a></li>
-			</ul>
-			<h2>Users</h2>
-			<h4>Table here...</h4>
-		</>
-	)
-};
+	it("has an input for password",()=>{
+		render(<Project />);
+		const password = screen.getByTestId("password");
+		expect(password).toBeInTheDocument();
+	});
 
-export default Dashboard;
+	it("has an input for email type password",()=>{
+		render(<Project />);
+		const input = screen.getByTestId("password");
+		expect(input).toBeInTheDocument();
+		expect(input).toHaveAttribute("type", "password");
+	});
+});
+
+describe("Checking validity of input",()=>{
+
+	afterEach(cleanup);
+
+	it("Check the email data input", () => {		
+		render(<Project />);
+		const input1 = screen.getByTestId("email");
+		fireEvent.change(input1, { target: { value: "eve.holt@reqres.in" }});
+		expect(input1.value).toBe("eve.holt@reqres.in");
+	});
+
+	it("Check the password data input", () => {		
+		render(<Project />);
+		const input2 = screen.getByTestId("password");
+		fireEvent.change(input2, { target: { value: "cityslicka" }});
+		expect(input2.value).toBe("cityslicka");
+	});
+
+	it("Check if valid user with token", async () => {   
+		render(<Project />);
+		await waitFor(() => {
+		    expect(screen.findByText('QpwL5tke4Pnpja7X4')).toBeTruthy() 
+		})
+	});
+});
+
+describe("Checking validity of wrong input",()=>{
+
+	afterEach(cleanup);
+
+	it("should check if input username fails", () => {		
+		render(<Project />);
+		const input1 = screen.getByTestId("email");
+		fireEvent.change(input1, { target: { value: "www" }});
+		expect(input1.value).not.toBe(true);
+	});
+
+	it("should check if input password fails", () => {		
+		render(<Project />);
+		const input2 = screen.getByTestId("password");
+		fireEvent.change(input2, { target: { value: "wwww" }});
+		expect(input2.value).not.toBe(true);
+	});
+});
